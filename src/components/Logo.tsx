96aa1c606a141b043b10a7e1caddb46ee9a8924c
novohit <zwx_info@163.com>
2023-11-23 21:24:46 +0800

    feat: Logo 组件

---
+++ src/components/Logo.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react';
+import { Space, Typography } from 'antd';
+import { FormOutlined } from '@ant-design/icons';
+import styles from './Logo.module.scss';
+import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
+
+const Logo: FC = () => {
+  return (
+    <div className={styles.container}>
+      <Link to="/">
+        <Space direction="horizontal">
+          <Title>
+            <FormOutlined />
+          </Title>
+          <Title>问卷星</Title>
+        </Space>
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,13 +2,16 @@ import Layout, { Content, Footer, Header } from 'antd/es/layout/layout';
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from './MainLayout.module.scss';
+import Logo from '../components/Logo';
 
 const MainLayout: FC = () => {
   return (
     <Layout>
       <Header className={styles.header}>
-        <div className={styles.left}>Logo</div>
-        <div className={styles.right}>登录</div>
+        <div className={styles.left}>
+          <Logo />
+        </div>
+        <div className={styles.right}>登录/用户信息</div>
       </Header>
       <Content className={styles.main}>
         <Outlet />
