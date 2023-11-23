8428afc71b85026cb67c14e7330e38a2220ab2a6
novohit <zwx_info@163.com>
2023-11-23 16:25:26 +0800

    refactor: antd Layout 的使用

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,20 @@
+import Layout, { Content, Footer, Header } from 'antd/es/layout/layout';
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
+import styles from './MainLayout.module.scss';
 
 const MainLayout: FC = () => {
   return (
-    <div>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>Logo</div>
+        <div className={styles.right}>登录</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </div>
+      </Content>
+      <Footer className={styles.footer}>Copyright © 2023-present novo.</Footer>
+    </Layout>
   );
 };
 
