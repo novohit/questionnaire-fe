01eaccf5aa453bfe99adc5c6764fdf51dc563d6d
novohit <zwx_info@163.com>
2023-11-23 16:39:00 +0800

    feat: antd Button

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,19 +1,33 @@
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from './ManageLayout.module.scss';
+import { Button, Divider, Space } from 'antd';
+import {
+  BarsOutlined,
+  DeleteOutlined,
+  PlusOutlined,
+  StarOutlined,
+} from '@ant-design/icons';
 
 const ManageLayout: FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <p>ManageLayout left</p>
-        <button>创建问卷</button>
-        <br />
-        <a href="#">我的问卷</a>
-        <br />
-        <a href="#">星标问卷</a>
-        <br />
-        <a href="#">回收站</a>
+        <Space direction="vertical">
+          <Button type="primary" icon={<PlusOutlined />}>
+            创建问卷
+          </Button>
+          <Divider />
+          <Button type="default" icon={<BarsOutlined />}>
+            我的问卷
+          </Button>
+          <Button type="default" icon={<StarOutlined />}>
+            星标问卷
+          </Button>
+          <Button type="default" icon={<DeleteOutlined />}>
+            回收站
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
