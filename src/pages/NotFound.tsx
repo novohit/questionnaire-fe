47c7a6159dd5234c7ac8deb3d6f911c1b7c7dee9
novohit <zwx_info@163.com>
2023-11-24 16:32:00 +0800

    feat: 404 页面

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,27 @@
+import { Button, Result } from 'antd';
 import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { HOME_PATH } from '../router';
 
 const NotFound: FC = () => {
+  const nav = useNavigate();
+
   return (
-    <>
-      <p>NotFound</p>
-    </>
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button
+          type="primary"
+          onClick={() => {
+            nav(HOME_PATH);
+          }}
+        >
+          Back Home
+        </Button>
+      }
+    />
   );
 };
 
