1bbc03d9fbd5bad2bdba07366e5a32758b803e9d
novohit <zwx_info@163.com>
2023-11-26 16:25:15 +0800

    feat: 注册表单校验

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -34,13 +34,48 @@ const Register: FC = () => {
           wrapperCol={{ span: 16 }}
           onFinish={onFinish}
         >
-          <Form.Item label="用户名" name="username">
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              {
+                type: 'string',
+                min: 4,
+                max: 20,
+                message: '字符长度在 4-20 之间',
+              },
+              { pattern: /^\w+$/, message: '只能为字母数学或下划线' },
+            ]}
+            validateFirst={true} // 当某一规则校验不通过时，是否停止剩下的规则的校验
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="密码" name="password">
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: '请输入密码' }]}
+          >
             <Input.Password />
           </Form.Item>
-          <Form.Item label="确认密码" name="rePassword">
+          <Form.Item
+            label="确认密码"
+            name="rePassword"
+            dependencies={['password']} // 依赖于 password ，password 变化，会重新触发 validator
+            rules={[
+              { required: true, message: '请确认密码' },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  } else {
+                    return Promise.reject(new Error('密码不一致'));
+                  }
+                },
+              }),
+            ]}
+            validateFirst={true}
+          >
             <Input.Password />
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 6, span: 18 }}>
