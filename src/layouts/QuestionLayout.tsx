87b6778ad01e683f037c66cdfc3b1ae4d436bed6
novohit <zwx_info@163.com>
2023-12-14 21:03:32 +0800

    style: 问卷编辑页画布布局

--- src/layouts/QuestionLayout.tsx
+++ src/layouts/QuestionLayout.tsx
@@ -3,12 +3,9 @@ import { Outlet } from 'react-router-dom';
 
 const QuestionLayout: FC = () => {
   return (
-    <>
-      <p>Question Layout</p>
-      <div>
-        <Outlet />
-      </div>
-    </>
+    <div>
+      <Outlet />
+    </div>
   );
 };
 
--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { FC } from 'react';
 import useLoadQuestion from '../../../hooks/useLoadQuestion';
+import styles from './index.module.scss';
 
 const Edit: FC = () => {
   // 用 hooks 抽取公共逻辑
@@ -7,8 +9,20 @@ const Edit: FC = () => {
 
   return (
     <>
-      <p>Edit</p>
-      {loading ? <p>loading...</p> : <p>{JSON.stringify(question)}</p>}
+      <div className={styles.container}>
+        <div>Edit Header</div>
+        <div className={styles['content-wrapper']}>
+          <div className={styles.content}>
+            <div className={styles.left}>left</div>
+            <div className={styles.main}>
+              <div className={styles['canvas-wrapper']}>
+                <div style={{ height: '900px' }}>画布测试滚动</div>
+              </div>
+            </div>
+            <div className={styles.right}>right</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
