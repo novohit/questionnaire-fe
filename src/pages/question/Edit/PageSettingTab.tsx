a64c1262823f8f7f74111d48044dbb26d2764079
novohit <zwx_info@163.com>
2024-01-01 13:13:28 +0800

    feat: 动态切换Tab

---
+++ src/pages/question/Edit/PageSettingTab.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const PageSettingTab: FC = () => {
+  return (
+    <>
+      <p>页面设置</p>
+    </>
+  );
+};
+
+export default PageSettingTab;
--- src/pages/question/Edit/RightPanel.tsx
+++ src/pages/question/Edit/RightPanel.tsx
@@ -1,40 +1,64 @@
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons';
 import { Tabs, TabsProps } from 'antd';
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import ComponentPropsTab from './ComponentPropsTab';
+import PageSettingTab from './PageSettingTab';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store';
+
+enum TabKey {
+  COMPONENT_PROPS = '1',
+  PAGE_SETTING = '2',
+}
+
+const items: TabsProps['items'] = [
+  {
+    key: TabKey.COMPONENT_PROPS,
+    label: (
+      <span>
+        <FileTextOutlined />
+        属性
+      </span>
+    ),
+    children: <ComponentPropsTab />,
+    // icon属性用不了？
+  },
+  {
+    key: TabKey.PAGE_SETTING,
+    label: (
+      <span>
+        <SettingOutlined />
+        页面设置
+      </span>
+    ),
+    children: <PageSettingTab />,
+  },
+];
 
 const RightPanel: FC = () => {
-  const items: TabsProps['items'] = [
-    {
-      key: '1',
-      label: (
-        <span>
-          <FileTextOutlined />
-          属性
-        </span>
-      ),
-      children: <ComponentPropsTab />,
-      // icon属性用不了？
-    },
-    {
-      key: '2',
-      label: (
-        <span>
-          <SettingOutlined />
-          页面设置
-        </span>
-      ),
-      children: <div>页面设置</div>,
-    },
-  ];
+  const componentsState = useSelector(
+    (state: RootState) => state.componentsState
+  );
+  const { selectedId } = componentsState;
+  const [activeKey, setActiveKey] = useState(TabKey.PAGE_SETTING);
+
+  // 监听selectedId 动态切换Tab
+  useEffect(() => {
+    if (selectedId) {
+      setActiveKey(TabKey.COMPONENT_PROPS);
+    } else {
+      setActiveKey(TabKey.PAGE_SETTING);
+    }
+  }, [selectedId]);
 
   const onChange = (key: string) => {
-    console.log(key);
+    const ky = key as TabKey;
+    setActiveKey(ky);
   };
 
   return (
     <div>
-      <Tabs defaultActiveKey="1" centered items={items} onChange={onChange} />
+      <Tabs activeKey={activeKey} centered items={items} onChange={onChange} />
     </div>
   );
 };
